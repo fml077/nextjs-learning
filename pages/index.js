faeb4d5b2c10107b1b2cc888ff214b7be4099892
fml077 <2320758400@qq.com>
2019-03-07 17:15:40 +0800

    demo03-fetchData

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,32 @@
 import Layout from '../components/layout/mainLayout'
+
 import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-const PostLink = (props) => (
-  <li>
-      <Link as={`p/${props.id}`} href={`/post?title=${props.title}`}>
-          <a>{props.title}</a>
-      </Link>
-  </li>
-)
-
-export default () => (
+const Index = (props) => (
   <Layout>
-      <h1>My blog</h1>
+      <h1>Batman TV Shows</h1>
       <ul>
-          <PostLink id='hello-nextjs' title='Hello next.js'/>
-          <PostLink id='learn-nextjs' title='Learn next.js'/>
-          <PostLink id='deploy-nextjs' title='Deploy app with me'/>
+          {props.shows.map(({show}) => (
+            <li key={show.id}>
+                <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                    <a>{show.name}</a>
+                </Link>
+            </li>
+          ))}
       </ul>
   </Layout>
 )
+
+Index.getInitialProps = async function() {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    const data = await res.json()
+
+    console.log(`Show data fetched. Count: ${data.length}`)
+
+    return {
+        shows: data
+    }
+}
+
+export default Index
--- pages/post.js
+++ pages/post.js
@@ -1,12 +1,33 @@
 import { withRouter } from 'next/router'
 import Layout from '../components/layout/mainLayout'
+import fetch from 'isomorphic-unfetch'
 
-const Page = withRouter((props) => (
+const Post = (props) => (
   <Layout>
-      <h1>{props.router.query.title}</h1>
-      <p>This is the blog post content</p>
+      <h1>{props.show.name}</h1>
+      <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+      <img src={props.show.image.medium}/>
   </Layout>
-))
+)
+
+Post.getInitialProps = async function (context) {
+    console.log('context:', context)
+    const { id } = context.query
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+    const show = await res.json()
+
+    console.log(`fetched show: ${show.name}`)
+
+    return { show }
+}
+
+export default Post
+// const Page = withRouter((props) => (
+//   <Layout>
+//       <h1>{props.router.query.title}</h1>
+//       <p>This is the blog post content</p>
+//   </Layout>
+// ))
 
 // 或者下面这种方式 再抽离出来一个Content组件
 // const Content = withRouter((props) => (
@@ -21,4 +42,4 @@ const Page = withRouter((props) => (
 //   </Layout>
 // )
 
-export default Page
+// export default Page
